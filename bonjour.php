99e6b0d5b377b4f32f8c3364a30c13cd8b0a238f
benboubekeur <boumedyen121@gmail.com>
2015-10-01 23:57:28 +0200

    Just Testing

--- bonjour.php
+++ bonjour.php
@@ -2,11 +2,13 @@
 // isset() : Cheking wheter an element existe or NOt
 if (isset ( $_GET ['nom'] )  {
 	if  isset(($_GET['prenom'])) {
-		# code...
-		echo 'Bonjour' . $_GET ['nom'] . ' ' . $_GET ['prenom'];
-	}
+	// code...
+	echo 'Bonjour' . $_GET ['nom'] . ' ' . $_GET ['prenom'];
+}
 	
 }
 //I'm trying CVS using Git Hub
+//I'm trying CVS using Git Hub
 ?>
+<?php ?>
 	
