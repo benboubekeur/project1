51e86d69736c99a20dbe02367fc590a77c990330
benboubekeur <boumedyen121@gmail.com>
2015-10-01 23:49:23 +0200

    My Second  Commit

--- bonjour.php
+++ bonjour.php
@@ -1,8 +1,12 @@
 <?php
 // isset() : Cheking wheter an element existe or NOt
-if (isset ( $_GET ['nom'] ) and isset ( $_GET ['prenom'] )) {
-	echo 'Bonjour' . $_GET ['nom'] . ' ' . $_GET ['prenom'];
+if (isset ( $_GET ['nom'] )  {
+	if  isset(($_GET['prenom'])) {
+		# code...
+		echo 'Bonjour' . $_GET ['nom'] . ' ' . $_GET ['prenom'];
+	}
+	
 }
-
+//I'm trying CVS using Git Hub
 ?>
 	
